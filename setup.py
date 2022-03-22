3826ce66dca7040133ced8e1df5c5f61ae9e2487
Bruno Mendes <brod56@sapo.pt>
2022-03-22 10:05:26 +0000

    Add boilerplate

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="traffic_signaling",
+    version="0.1.1",
+    author="feup-iart",
+    python_requires='>=3.6',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=required,
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
