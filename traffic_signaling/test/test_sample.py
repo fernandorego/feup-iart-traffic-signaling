84838ad64f06c7b252957f9c06be39e2380dde63
Fernando Rego <fernandoluisrego@gmail.com>
2022-03-30 09:30:41 +0100

    Add a new test && add data and out folders

--- traffic_signaling/test/test_sample.py
+++ traffic_signaling/test/test_sample.py
@@ -1,14 +1,20 @@
 from traffic_signaling.src.model.city import City
 from traffic_signaling.src.model.schedule import Schedule
 
-example_city = City.from_input('traffic_signaling/asset/ain.txt')
+a_city = City.from_input('traffic_signaling/asset/data/a.txt')
+b_city = City.from_input('traffic_signaling/asset/data/b.txt')
 
 
 def test_example_solution1():
-    schedule = Schedule.from_input('traffic_signaling/asset/aout1.txt')
-    assert(schedule.evaluate(example_city) == 1002)
+    schedule = Schedule.from_input('traffic_signaling/asset/out/a1.txt')
+    assert(schedule.evaluate(a_city) == 1002)
 
 
 def test_example_solution2():
-    schedule = Schedule.from_input('traffic_signaling/asset/aout2.txt')
-    assert(schedule.evaluate(example_city) == 2002)
+    schedule = Schedule.from_input('traffic_signaling/asset/out/a2.txt')
+    assert(schedule.evaluate(a_city) == 2002)
+
+
+def test_example_solution3():
+    schedule = Schedule.from_input('traffic_signaling/asset/out/b1.txt')
+    assert(schedule.evaluate(b_city) == 707937)
