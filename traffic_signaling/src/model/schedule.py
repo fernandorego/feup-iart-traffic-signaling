d5912c5e90e2a57a82a2a13585a06e219e1f4c4a
Bruno Mendes <brod56@sapo.pt>
2022-03-31 11:38:11 +0100

    Simplify green light schedule

--- traffic_signaling/src/model/schedule.py
+++ traffic_signaling/src/model/schedule.py
@@ -45,16 +45,19 @@ class Schedule:
                 car.id)
             car_position[car.id] = city.street_length(car_path[car.id][0])
 
-        # copy schedule
-        next_lights = copy.deepcopy(self.schedule)
+        # setup green_lights
+        green_lights = {}
+        for intersection_id in self.schedule:
+            green_lights[intersection_id] = []
+            for name, time in self.schedule[intersection_id]:
+                green_lights[intersection_id].extend(
+                    [name for _ in range(time)])
 
         score = 0
         car_ids = [car.id for car in city.cars]
-        for _ in range(city.duration+1):
-            # update car positions
+        for current_time in range(city.duration+1):
             for car_id in car_ids:
                 if car_path[car_id] == []:
-                    score += 1
                     continue
                 street_name = car_path[car_id][0]
                 street_length = city.street_length(street_name)
@@ -62,31 +65,18 @@ class Schedule:
                     car_position[car_id] += 1
                     if car_position[car_id] == street_length:
                         if car_path[car_id][1:] == []:
-                            score += city.bonus
+                            score += city.bonus + city.duration - current_time
                             car_path[car_id] = []
                             break
                         street_queue[street_name].append(car_id)
                 if car_position[car_id] == street_length and street_queue[street_name][0] == car_id:
                     for intersection_id in self.schedule:
-                        if next_lights[intersection_id][0][0] != street_name:
+                        if green_lights[intersection_id][current_time % len(green_lights[intersection_id])] != street_name:
                             continue
                         street_queue[street_name] = street_queue[street_name][1:]
                         car_position[car_id] = 0
                         car_path[car_id] = car_path[car_id][1:]
                         break
-
-            # update intersection lights
-            for intersection_id in self.schedule:
-                incoming_street_name, duration = next_lights[intersection_id][0]
-                duration -= 1
-                if duration == 0:
-                    next_lights[intersection_id] = next_lights[intersection_id][1:]
-                    if next_lights[intersection_id] == []:
-                        next_lights[intersection_id] = self.schedule[intersection_id].copy(
-                        )
-                else:
-                    next_lights[intersection_id][0] = (
-                        incoming_street_name, duration)
         return score
 
     def __str__(self):
