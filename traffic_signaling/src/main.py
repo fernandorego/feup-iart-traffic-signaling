601bf59b47b68747badd340f7a5671124fe3e8cf
Bruno Mendes <brod56@sapo.pt>
2022-03-31 18:06:31 +0100

    Optimize intersection access

--- traffic_signaling/src/main.py
+++ traffic_signaling/src/main.py
@@ -3,15 +3,10 @@ from model.schedule import Schedule
 
 if __name__ == '__main__':
     city = City.from_input('traffic_signaling/asset/data/a.txt')
-    print(city)
     schedule = Schedule.from_input('traffic_signaling/asset/out/a1.txt')
     # print(schedule)
-    print()
     print(schedule.evaluate(city))
-    print()
 
-    schedule1 = Schedule.from_input('traffic_signaling/asset/out/a2.txt')
+    schedule1 = Schedule.from_input('traffic_signaling/asset/out/a3.txt')
     # print(schedule1)
-    print()
     print(schedule1.evaluate(city))
-    print()
--- traffic_signaling/src/model/schedule.py
+++ traffic_signaling/src/model/schedule.py
@@ -31,7 +31,7 @@ class Schedule:
         return schedule
 
     def evaluate(self, city: City):
-        # street queues
+        # street
         street_queue = {}
         for s in city.streets:
             street_queue[s.name] = []
@@ -40,20 +40,23 @@ class Schedule:
         car_path = {}
         car_position = {}
         for car in city.cars:
-            car_path[car.id] = [s.name for s in car.path]
-            street_queue[car_path[car.id][0]].append(
+            car_path[car.id] = [s for s in car.path]
+            street_queue[car_path[car.id][0].name].append(
                 car.id)
-            car_position[car.id] = city.street_length(car_path[car.id][0])
+            car_position[car.id] = car_path[car.id][0].length
 
         # setup green_lights
         green_lights = {}
         green_cycle = {}
+        street_intersection = {}
         for intersection_id in self.schedule:
             green_lights[intersection_id] = []
             for name, time in self.schedule[intersection_id]:
                 green_lights[intersection_id].extend(
                     [name for _ in range(time)])
             green_cycle[intersection_id] = len(green_lights[intersection_id])
+            for incoming_street in city.intersections[intersection_id].incoming_streets:
+                street_intersection[incoming_street.name] = intersection_id
 
         score = 0
         car_ids = [car.id for car in city.cars]
@@ -61,24 +64,22 @@ class Schedule:
             for car_id in car_ids:
                 if car_path[car_id] == []:
                     continue
-                street_name = car_path[car_id][0]
-                street_length = city.street_length(street_name)
-                if car_position[car_id] < street_length:
+                street = car_path[car_id][0]
+                if car_position[car_id] < street.length:
                     car_position[car_id] += 1
-                    if car_position[car_id] == street_length:
+                    if car_position[car_id] == street.length:
                         if car_path[car_id][1:] == []:
                             score += city.bonus + city.duration - current_time
                             car_path[car_id] = []
                             continue
-                        street_queue[street_name].append(car_id)
-                if car_position[car_id] == street_length and street_queue[street_name][0] == car_id:
-                    for intersection_id in self.schedule:
-                        if green_lights[intersection_id][current_time % green_cycle[intersection_id]] != street_name:
-                            continue
-                        street_queue[street_name] = street_queue[street_name][1:]
-                        car_position[car_id] = 0
-                        car_path[car_id] = car_path[car_id][1:]
-                        break
+                        street_queue[street.name].append(car_id)
+                if car_position[car_id] == street.length and street_queue[street.name][0] == car_id:
+                    intersection_id = street_intersection[street.name]
+                    if green_lights[intersection_id][current_time % green_cycle[intersection_id]] != street.name:
+                        continue
+                    street_queue[street.name] = street_queue[street.name][1:]
+                    car_position[car_id] = 0
+                    car_path[car_id] = car_path[car_id][1:]
         return score
 
     def __str__(self):
