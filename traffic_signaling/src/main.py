805a87e4f94970eb61a11e4a3d2e902bdfa7fea8
David Preda <dtpreda30@gmail.com>
2022-04-06 17:17:56 +0100

    Update main

--- traffic_signaling/src/main.py
+++ traffic_signaling/src/main.py
@@ -1,13 +1,16 @@
 import time
 from model.city import City
 from model.schedule import Schedule
+from algorithm.common import generate_random_solution
+from algorithm.genetics import genetic_algorithm
 
-if __name__ == '__main__':
-    city = City.from_input('traffic_signaling/asset/data/d.txt')
-    print("read city")
-    schedule = Schedule.from_input('traffic_signaling/asset/out/d1.txt')
-    print("read schedule")
-    t1 = time.time()
-    print(schedule.evaluate(city))
-    t2 = time.time()
-    print(f'Simulation time: {t2-t1}')
+if __name__ == "__main__":
+    city = City.from_input("traffic_signaling/asset/data/e.txt")
+    genetic_algorithm(city, 50, 30, 0.5)
+    # print("read city")
+    # schedule = Schedule.from_input('traffic_signaling/asset/out/d1.txt')
+    # print("read schedule")
+    # t1 = time.time()
+    # print(schedule.evaluate(city))
+    # t2 = time.time()
+    # print(f'Simulation time: {t2-t1}')
