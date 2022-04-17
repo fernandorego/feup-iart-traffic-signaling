cbf87117f093c22c89244aeddcc372e56bc85ed3
David Preda <dtpreda30@gmail.com>
2022-04-17 22:57:34 +0100

    Add new mutate operator

--- traffic_signaling/src/algorithm/genetics.py
+++ traffic_signaling/src/algorithm/genetics.py
@@ -37,14 +37,14 @@ def genetic_algorithm_process(
     for _ in range(number_of_generations):
         for schedule in population:
             if random() <= mutation_chance:
-                schedule = mutate_intersection(city, schedule)
+                schedule = mutate_single_street(city, schedule)
                 schedule.evaluate(city)
 
-        for index in range(int(population_size / 4)):
+        for index in range(int(len(population) / 4)):
             best_parent = population[index]
             second_parent_index = index
             while second_parent_index == index:
-                second_parent_index = randint(0, population_size - 1)
+                second_parent_index = randint(0, len(population) - 1)
 
             random_parent = population[second_parent_index]
 
@@ -139,14 +139,14 @@ def genetic_algorithm(
     for _ in range(number_of_generations):
         for schedule in population:
             if random() <= mutation_chance:
-                schedule = mutate_random_intersection(city, schedule)
+                schedule = mutate_single_street(city, schedule)
                 schedule.evaluate(city)
 
-        for index in range(int(population_size / 4)):
+        for index in range(int(len(population) / 4)):
             best_parent = population[index]
             second_parent_index = index
             while second_parent_index == index:
-                second_parent_index = randint(0, population_size - 1)
+                second_parent_index = randint(0, len(population) - 1)
 
             random_parent = population[second_parent_index]
 
@@ -251,4 +251,36 @@ def mutate_random_intersection(city: City, schedule: Schedule):
         schedule.schedule[intersection_id] += [
             street.name for _ in range(intersection_schedule[index])
         ]
+    return schedule
+
+
+def mutate_single_street(city: City, schedule: Schedule):
+    intersections = list(city.intersections.keys())
+    intersection_id = intersections[randint(0, len(intersections) - 1)]
+    current_intersection_schedule = schedule.schedule[intersection_id]
+
+    current_intersection_schedule_dict = {
+        street_name: current_intersection_schedule.count(street_name)
+        for street_name in set(current_intersection_schedule)
+    }
+
+    streets = list(current_intersection_schedule_dict.items())
+    if len(streets) > 1:
+        street, street_time = streets[randint(0, len(streets) - 1)]
+    else:
+        street, street_time = streets[0]
+
+    remaining_time = city.duration - (
+        sum(list(current_intersection_schedule_dict.values())) - street_time
+    )
+
+    print(current_intersection_schedule_dict, street, remaining_time)
+    current_intersection_schedule_dict[street] = randint(0, remaining_time)
+
+    schedule.schedule[intersection_id] = [
+        street_name
+        for street_name, street_time in current_intersection_schedule_dict.items()
+        for _ in range(street_time)
+    ]
+
     return schedule
