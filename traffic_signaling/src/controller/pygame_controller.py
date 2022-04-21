b9af206502e47ac9cd8d9c447cfe758198f2f6ec
Fernando Rego <fernandoluisrego@gmail.com>
2022-04-21 11:39:39 +0100

    Add pygame exit condition

--- traffic_signaling/src/controller/pygame_controller.py
+++ traffic_signaling/src/controller/pygame_controller.py
@@ -2,8 +2,10 @@ import pygame
 from model.city import City
 from controller.city_controller import CityController
 from model.schedule import Schedule
+from time import time, sleep
 
 WINDOW_SIZE = (1300, 800)
+FPS = 30
 
 
 class PygameController:
@@ -20,15 +22,24 @@ class PygameController:
     def quit_pygame(self) -> None:
         pygame.quit()
 
-    def is_running(self) -> bool:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.quit_pygame()
-
-    def simulate(self, schedule) -> None:
+    def simulate(self, schedule: Schedule) -> None:
         self.city_controller.set_schedule(schedule)
         if self.city_controller.simulate() == 1:
             self.quit_pygame()
+        self.wait_for_exit()
 
     def update(self) -> None:
         self.city_controller.update()
+
+    def wait_for_exit(self) -> None:
+        start = time()
+        while self.is_running():
+            sleep(max(1.0/FPS - (time() - start), 0))
+            continue
+        self.quit_pygame()
+
+    def is_running(self) -> bool:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                return False
+        return True
