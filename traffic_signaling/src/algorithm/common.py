59b1204f664bb0db976c2e20407b6b84dbd61364
David Preda <dtpreda30@gmail.com>
2022-04-23 23:50:13 +0100

    Update distributed_sum_permutation docstring

--- traffic_signaling/src/algorithm/common.py
+++ traffic_signaling/src/algorithm/common.py
@@ -49,7 +49,7 @@ def distributed_random_sum_permutation(length: int, perm_sum: int):
 def distributed_sum_permutation(length: int, perm_sum: int):
     """
     Creates a list of lenght length of integers with a sum inferior than perm_sum.
-    If length is larger that perm_sum, generates a random permutation of integer values.
+    If length is larger that perm_sum, generates a random permutation of integer values, , with the same sum condition.
     Else, the list is filled with 1s.
 
     Parameters:
